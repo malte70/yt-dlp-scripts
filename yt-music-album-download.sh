22ef52c25a2735f26f06322c7678979f6d5c3699
Malte Bublitz <malte@rolltreppe3.de>
2025-08-31 05:07:55 +0200

    Configure output location using a config file
    Read the output filename template for yt-dlp as well as the destination
    for your music files from a configuration file (.env).
    The default config included in this repository supports XDG User Dirs
    on GNU/Linux, and the weird Music folder hierarchy on recent macOS
    versions (~/Music/Music/Media.localized/Music).

--- yt-music-album-download.sh
+++ yt-music-album-download.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Load configuration file (Git repository, file ".env")
+# Allow calling this script using a symlink. On my computer, I run this script using the symlink ~/Music/download-youtube-music.sh,
+# which points to the cloned Git repo in ~/code/yt-dlp-scripts.
+APP_DIR=$(dirname $(realpath $0))
+
+echo "Loading configuration from .env..."
+source "$APP_DIR/.env" \
+	|| exit 2
+if [[ -d "$MUSIC_DIR" ]]; then
+	echo "Using Music Directory $MUSIC_DIR..."
+	cd "$MUSIC_DIR"
+fi
+
 ### Script for downloading albums from Youtube Music ##########
 ### Usage: ./yt-music-album-download.sh <youtube music url> ###
 
@@ -17,29 +30,36 @@ jsondata=`yt-dlp -j --playlist-items 1 $1`
 jq_release_year_1=`echo $jsondata | jq -r '.release_year'`
 jq_release_date=`echo $jsondata | jq -r '.release_date'`
 if [ $jq_release_date != 'null' ]; then
-        jq_release_year_2=${jq_release_date::-4};
-        year=$((jq_release_year_1<jq_release_year_2?jq_release_year_1:jq_release_year_2));
+	jq_release_year_2=${jq_release_date::-4};
+	year=$((jq_release_year_1<jq_release_year_2?jq_release_year_1:jq_release_year_2));
 else
-        year=$jq_release_year_1;
+	year=$jq_release_year_1;
 fi
 
 # Grabbing the artist then removing any superfluous information after the first comma. Some artists put every band memember into the artist field.
 jq_artist=`echo $jsondata | jq -r '.artist'`
 artist=${jq_artist%%,*}
 
+# Dirty hack to use the sanitized artist name from above in the configuration variable $OUTPUT_TEMPLATE.
+output_tpl="${OUTPUT_TEMPLATE//§artist/$artist}"
+
 echo "Album information retrieved..."
 # Pass to yt-dlp and begin download all the music!
-yt-dlp  --ignore-errors \
-        --format "(bestaudio[acodec^=opus]/bestaudio)/best" \
-        --extract-audio \
-        --audio-format mp3 \
-        --audio-quality 0 \
-        --parse-metadata "playlist_index:%(track_number)s" \
-        --parse-metadata ":(?P<webpage_url>)" \
-        --parse-metadata ":(?P<synopsis>)" \
-        --parse-metadata ":(?P<description>)" \
-        --add-metadata \
-        --postprocessor-args "-metadata date='${year}' -metadata artist=\"${artist}\" -metadata album_artist=\"${artist}\"" \
-        --embed-thumbnail \
-        --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" \
-        -o "$artist - %(album)s/%(playlist_index)s - %(title)s.%(ext)s" "$1"
+#DEBUG: echo \
+yt-dlp \
+	--ignore-errors \
+	--format "(bestaudio[acodec^=opus]/bestaudio)/best" \
+	--extract-audio \
+	--audio-format mp3 \
+	--audio-quality 0 \
+	--parse-metadata "playlist_index:%(track_number)s" \
+	--parse-metadata ":(?P<webpage_url>)" \
+	--parse-metadata ":(?P<synopsis>)" \
+	--parse-metadata ":(?P<description>)" \
+	--add-metadata \
+	--postprocessor-args "-metadata date='${year}' -metadata artist=\"${artist}\" -metadata album_artist=\"${artist}\"" \
+	--embed-thumbnail \
+	--ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" \
+	-o "${output_tpl}" \
+	"$1"
+
